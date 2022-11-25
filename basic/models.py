c5c3d799766387f14083183d3ba97e9bac50cbab
Mohammad Raihan Rashid <raihanrsd@icloud.com>
2022-11-25 15:11:30 +0600

    Raihan's first commit & Mehedi is dumb.

--- basic/models.py
+++ basic/models.py
@@ -5,3 +5,33 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     pass
+
+class Finance(models.Model):
+    user = models.ForeignKey(User, on_delete = models.CASCADE)
+    income = models.DecimalField(max_digits=8, decimal_places=2)
+    income_cycle = models.IntegerField(default=30)
+    balance = models.DecimalField(max_digits=8, decimal_places=2, default=0)
+    saving_goal = models.DecimalField(max_digits=8, decimal_places = 2, default=0)
+    goal = models.IntegerField(default=30)
+    daily_cost_estimate = models.DecimalField(max_digits = 6, decimal_places=2, default=0)
+    total_expenditure = models.DecimalField(max_digits = 8, decimal_places=2, default=0)
+    add_money = models.DecimalField(max_digits = 8, decimal_places = 2, default = 0)
+    
+    def __str__(self):
+        return f"{self.income} {self.balance} {self.goal} {self.total_expenditure}"
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    def __str__(self):
+        return f"{self.name}"
+
+
+
+class DailyExpenditure(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    stuff = models.CharField(max_length=100)
+    cost = models.DecimalField(max_digits=8, decimal_places=2)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, blank=True, default= "none")
+    total_spending = models.DecimalField(max_digits=8, decimal_places=2)
+
--- basic/views.py
+++ basic/views.py
@@ -6,12 +6,54 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
 from django.contrib.auth.decorators import login_required
-from .models import User
+from .models import User, Finance, DailyExpenditure, Category
 
 # Create your views here.
 
+
+
+
+@login_required
+def add_record(request):
+    income = request.POST.get("income", None)
+    finance = Finance(user=request.user, income=income)
+    cycle = request.POST.get("cycle", None)
+    if cycle:
+        finance.income_cycle = cycle
+    balance = request.POST.get("balance", None)
+    if balance:
+        finance.balance = balance
+    saving = request.POST.get("saving", None)
+    if saving:
+        finance.saving_goal = saving
+    goal = request.POST.get("goal", None)
+    if goal:
+        finance.goal = goal
+    estimate = request.POST.get("estimate", None)
+    if estimate:
+        finance.daily_cost_estimate = estimate
+    finance.save()
+    daily_income = float(finance.income) / float(finance.income_cycle)
+    
+
+    return HttpResponseRedirect(reverse("index"))
+
+    
+
+
+
 def index(request):
-    return render(request, "basic/index.html")
+    finance = None
+    if request.user.is_authenticated:
+        print("no")
+        if Finance.objects.filter(user = request.user):
+            finance = Finance.objects.get(user=request.user)
+
+            print("yes")
+    print(finance)
+    return render(request, "basic/index.html",{
+        "finance": finance
+    })
 
 def login_view(request):
     logout(request)
