0864ba71059e7333b6e2fade6f727e393efc7eef
mehedi khan <lopingcard@gmail.com>
2022-11-30 00:13:05 +0600

    raihan cringe af

--- basic/static/basic/script.js
+++ basic/static/basic/script.js
@@ -25,3 +25,31 @@ function remove_item(x){
         para1.innerHTML = "Your total daily expenditure is " + finance.daily_expenditure;
     })
 }
+
+document.addEventListener('DOMContentLoaded', function(){
+    let api_btn = document.querySelector('#get-api-data-btn');
+    api_btn.addEventListener('click', function(){
+        console.log("works");
+        exchange_rates();
+    })
+})
+
+// exchange_rates();
+
+function exchange_rates() {
+        fetch('https://api.currencyapi.com/v3/latest?apikey=WFrsTGPjkGFnHgjFUek2eO0DXFNtL2zooJ4rE5NK')
+        .then(response => response.json())
+        .then(data => {
+            let from = document.getElementById('exchange_from').value;
+            let to = document.getElementById('exchange_to').value;
+
+            from_USD = data.data[from].value;
+            to_USD = data.data[to].value;
+              
+            let conversion_value = to_USD / from_USD;
+            api_div = document.getElementById('exchange-info');
+            api_div.innerHTML = `1 ${from} is ${conversion_value} ${to}`;
+        })
+
+}
+
